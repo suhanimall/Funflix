4bf05ff618d1067c30951964b3a6904f2bc6b9b9
suhanimall <76207467+suhanimall@users.noreply.github.com>
2023-07-16 16:32:04 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,7 +14,7 @@ const Home = ({ type }) => {
         const getRandomLists = async () => {
             try {
                 const res = await axios.get(
-                    `lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`, {
+                    `https://funflix-backend.onrender.com/api/lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`, {
                     headers: {
                         token:
                             "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
